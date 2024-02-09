9ef35a1b18f5096d99ac9455d77ed820ec4faf6d
AdrienneUsi <usiadrienne35@gmail.com>
2024-02-09 10:11:31 -0800

    finish exercise

---
+++ KanyeWestApiJSON/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWestApiJSON
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var kanyeQuotes = await QuoteGenerator.GetKanyeQuotes(5);
+            var ronQuotes = await QuoteGenerator.GetRonQuotes(5);
+
+            Console.WriteLine("Kanye and Ron's Conversation:");
+            Console.WriteLine("----------------------------");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye: '{kanyeQuotes[i]}'");
+                Console.WriteLine($"Ron: '{ronQuotes[i]}'");
+                Console.WriteLine("----------------------------");
+            }
+
+            await WeatherGenerator.GetCurrentWeather();
+        }
+    }
+}
